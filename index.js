1cdf9a8a13cb8402e9e5083975f2b3b50bff0301
raoffonom <raoffonom@icloud.com>
2020-04-12 14:42:04 +0300

    start

---
+++ index.js
@@ -0,0 +1,15 @@
+import { AppRegistry, YellowBox } from 'react-native'
+import App from './src'
+import { name as appName } from './app.json'
+
+YellowBox.ignoreWarnings([
+  'Warning: AsyncStorage',
+  'Warning: componentWillReceiveProps',
+  'RCTRootView cancelTouches',
+  'not authenticated',
+  'Sending `onAnimatedValueUpdate`'
+])
+
+//window.LOG_LEVEL = 'DEBUG'
+
+AppRegistry.registerComponent(appName, () => App)
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Amplify from '@aws-amplify/core'
+import * as Keychain from 'react-native-keychain'
+import { ThemeProvider, DarkTheme, LightTheme } from 'react-native-unicorn-uikit'
+import { useColorScheme } from 'react-native-appearance'
+import AppNavigator from './AppNavigator'
+import awsconfig from '../aws-exports'
+
+const MEMORY_KEY_PREFIX = '@MyStorage:'
+let dataMemory = {}
+
+class MyStorage {
+  static syncPromise = null
+
+  static setItem(key, value) {
+    Keychain.setGenericPassword(MEMORY_KEY_PREFIX + key, value)
+    dataMemory[key] = value
+    return dataMemory[key]
+  }
+
+  static getItem(key) {
+    return Object.prototype.hasOwnProperty.call(dataMemory, key) ? dataMemory[key] : undefined
+  }
+
+  static removeItem(key) {
+    Keychain.resetGenericPassword()
+    return delete dataMemory[key]
+  }
+
+  static clear() {
+    dataMemory = {}
+    return dataMemory
+  }
+}
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: false
+  },
+  storage: MyStorage
+})
+
+const App = () => {
+  const scheme = useColorScheme()
+  return (
+    <>
+      <ThemeProvider theme={scheme === 'dark' ? DarkTheme : LightTheme}>
+        <AppNavigator />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
